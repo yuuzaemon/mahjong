08848841462d0cc63ee8e9c721117499a3acad6b
yuuzaemon <99088426+yuuzaemon@users.noreply.github.com>
2024-02-02 09:48:51 +0900

    js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function clicked(tiles){
-    let clicked_tiles = tiles;
-    document.getElementById("")
+    let clicked_tiles = document.getElementById(tiles).innerHTML;
+    let text = document.getElementById("box").value;
+    document.getElementById("box").value = text + clicked_tiles;
 }
